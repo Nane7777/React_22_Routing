58e27f5416a5262ccff409e997455c6cefd6bea6
Nane <gemine_nicolas@hotmail.com>
2025-04-28 22:23:11 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+// const routeDefinitions = createRoutesFromElements(
+//     <Route>
+//         <Route path="/" component={<HomePage />} />
+//         <Route path="/products" component={<ProductsPage />} />
+//     </Route>
+// );
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout />,
+        children: [
+            {path: '/', element: <HomePage />},
+            {path: '/products', element: <ProductsPage />},
+        ],
+    },
+]);
+
+function App() {
+    return <RouterProvider router={router}/>;
+}
+
+export default App;
